a192e840fa47df3bcd0ab934810a1d6549b954a4
Andrea Idini <andrea.idini@matfys.lth.se>
2025-12-02 23:14:31 +0100

    start of multipage

---
+++ pages/02_Fees_keep_you_poor.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Fees keep you poor", layout="centered")
+
+st.title("Test")
---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title="The Financial Sandbox", layout="centered")
+
+st.title("📊 The Financial Sandbox")
+
+st.markdown("""
+Welcome! This is the interactive companion to my [**Substack Blog**](https://idini.substack.com).
+
+Here you will find interactive simulations useful to understand finance. You can go to [the blog](https://idini.substack.com) for explanation and context.
+Select a tool from the sidebar 👈 to get started.
+
+### Latest Tools:
+*   [**Egg or Chicken**](/Egg_or_Chicken): Visualize the Time Value of Money.
+*   [**Fees keep you poor**](/Fees_keep_you_poor): See how 1% fees destroy your retirement.
+""")
