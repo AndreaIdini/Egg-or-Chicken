071b4a58320ee36744dda68521c552c22c9683f1
Andrea Idini <andrea.idini@matfys.lth.se>
2025-12-07 23:42:01 +0100

    debugging streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.write("Hello World")
 # We wrap the python files in st.Page objects
 # 'url_path' allows you to set the deep link (e.g. /fees instead of /fees_logic)
 
-# intro_page = st.Page("tools/intro_page.py", title="Homepage", icon="🏠", default=True)
+intro_page = st.Page("tools/intro_page.py", title="Homepage", icon="🏠", default=True)
 
 # # Section: Basics
 # egg_page = st.Page("tools/01_Egg_or_Chicken.py", title="Egg vs Chicken", icon="🐣", url_path="tvm")
@@ -21,19 +21,19 @@ st.write("Hello World")
 
 # --- 3. CREATE NAVIGATION (SUBSECTIONS) ---
 # This dictionary structure creates the "Grouping" in the sidebar
-# pg = st.navigation({
-#     "Welcome": [intro_page],
-#     "The Basics": [egg_page, fee_page] #,
-#     # "Advanced Modeling": [fee_page, mc_page]
-# })
+pg = st.navigation({
+    "Welcome": [intro_page]#,
+    # "The Basics": [egg_page, fee_page] #,
+    # "Advanced Modeling": [fee_page, mc_page]
+})
 
 # --- 4. SHARED SIDEBAR ELEMENTS ---
 # Anything you write here will appear on EVERY page
-# st.sidebar.text("Made by Andrea Idini")
-# # st.logo("assets/logo.png") # Adds a nice image at the top left
+st.sidebar.text("Made by Andrea Idini")
+# st.logo("assets/logo.png") # Adds a nice image at the top left
 
-# # Optional: Add a "Subscribe" link for your Substack
-# st.sidebar.link_button("Subscribe to Substack", "https://idini.substack.com")
+# Optional: Add a "Subscribe" link for your Substack
+st.sidebar.link_button("Subscribe to Substack", "https://idini.substack.com")
 
-# # --- 5. RUN THE SELECTED PAGE ---
-# pg.run()
+# --- 5. RUN THE SELECTED PAGE ---
+pg.run()
